fe838611d8313ade18bde8ae9954b7cfb790c7f8
papgergo1997 <pap.gergo.1997@gmail.com>
2020-11-28 12:46:47 +0100

    input-problem

---
+++ js/calc.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const input = document.querySelector('.input');
+
+const btnSum = document.querySelector('.sum');
+const btnSub = document.querySelector('.sub');
+const btnMulti = document.querySelector('.multi');
+const btnDist = document.querySelector('.dist');
+
+const btnZero = document.querySelector('.zero');
+const btnDot = document.querySelector('.dot');
+const btnC = document.querySelector('.c');
+
+const btnOne = document.querySelector('.one');
+const btnTwo = document.querySelector('.two');
+const btnThree = document.querySelector('.three');
+const btnFour = document.querySelector('.four');
+const btnFive = document.querySelector('.five');
+const btnSix = document.querySelector('.six');
+const btnSeven = document.querySelector('.seven');
+const btnEight = document.querySelector('.eight');
+const btnNine = document.querySelector('.nine');
+
+const btnResult = document.querySelector('.result')
+
+
+
+
+btnOne.onclick = function() {
+input.value = 1;
+} 
+
+
